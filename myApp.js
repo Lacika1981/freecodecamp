f672b6138e56e51131ecc92a2475574a6fcdf99a
Glitch (robust-temperature) <none>
2018-09-17 21:42:45 +0000

    use .env file to store configuration option

--- myApp.js
+++ myApp.js
@@ -30,11 +30,19 @@ app.use(express.static(`${__dirname}/public`));
 
 /** 5) serve JSON on a specific route */
 
-app.get('/json', (req, res) => {
-  res.json({"message": "Hello json"});
-});
+// app.get('/json', (req, res) => {
+//   res.json({"message": "Hello json"});
+// });
 
 /** 6) Use the .env file to configure the app */
+
+app.get('/json', (req, res) => {
+  let message = {"message": "Hello json"};
+  if(process.env.MESSAGE_STYLE === 'uppercase') {
+    message.message = message.message.toUpperCase();
+  }
+  return res.json(message);
+})
  
  
 /** 7) Root-level Middleware - A logger */
